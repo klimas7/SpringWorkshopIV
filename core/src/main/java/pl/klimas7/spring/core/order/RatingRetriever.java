1f701925f7c1e71824363f700dcdd67d6840753e
klimas7 <klimas7@gmail.com>
2020-11-28 21:57:23 +0100

    Order example #4: change as ratings

--- core/src/main/java/pl/klimas7/spring/core/order/RatingRetriever.java
+++ core/src/main/java/pl/klimas7/spring/core/order/RatingRetriever.java
@@ -1,11 +1,11 @@
 package pl.klimas7.spring.core.order;
 
 import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Log
 @Component
@@ -14,13 +14,18 @@ public class RatingRetriever {
 //    @Qualifier("good")
     private final Rating rating;
 
-    public RatingRetriever(@Qualifier("good") Rating rating) {
+    private final List<Rating> ratings;
+
+    public RatingRetriever(@Qualifier("good") Rating rating,
+                           List<Rating> ratings) {
         this.rating = rating;
+        this.ratings = ratings;
     }
 
 
     @PostConstruct
     public void printRating() {
         log.info("Rating: " + rating.getRating());
+        ratings.forEach(r -> log.info("Rating: " + r.getRating() + " (" + r.getClass().getSimpleName() + ")"));
     }
 }
