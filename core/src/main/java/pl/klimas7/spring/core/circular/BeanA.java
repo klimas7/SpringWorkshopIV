a614aabae249288125a5ac590512bf7ac4502a6d
klimas7 <klimas7@gmail.com>
2020-11-28 22:44:47 +0100

    Circular example #3: Break

--- core/src/main/java/pl/klimas7/spring/core/circular/BeanA.java
+++ core/src/main/java/pl/klimas7/spring/core/circular/BeanA.java
@@ -1,12 +1,20 @@
 package pl.klimas7.spring.core.circular;
 
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Component;
 
+@Log
 @Component
 public class BeanA {
+    private static final String message = "BeanA";
     private final BeanB beanB;
 
     public BeanA(BeanB beanB) {
         this.beanB = beanB;
+        this.beanB.printMessage();
+    }
+
+    public void printMessage() {
+        log.info(message);
     }
 }
--- core/src/main/java/pl/klimas7/spring/core/circular/BeanB.java
+++ core/src/main/java/pl/klimas7/spring/core/circular/BeanB.java
@@ -1,13 +1,21 @@
 package pl.klimas7.spring.core.circular;
 
+import lombok.extern.java.Log;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+@Log
 @Component
 public class BeanB {
+    private static final String message = "BeanB";
     private final BeanA beanA;
 
     public BeanB(@Lazy BeanA beanA) {
         this.beanA = beanA;
+        this.beanA.printMessage();
+    }
+
+    public void printMessage() {
+        log.info(message);
     }
 }
