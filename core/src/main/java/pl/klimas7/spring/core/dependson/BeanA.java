4f362aec6b5a13f0ed1311a5e180817ff46392a0
klimas7 <klimas7@gmail.com>
2020-11-28 23:18:41 +0100

    Depends on #2

--- core/src/main/java/pl/klimas7/spring/core/dependson/BeanA.java
+++ core/src/main/java/pl/klimas7/spring/core/dependson/BeanA.java
@@ -1,10 +1,12 @@
 package pl.klimas7.spring.core.dependson;
 
 import lombok.extern.java.Log;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 @Log
 @Component
+@DependsOn("beanB")
 public class BeanA {
     public BeanA() {
         log.info("BeanA");
