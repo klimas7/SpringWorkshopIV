d59fb43d3f0f770129b7bdbff70186a870e25948
klimas7 <klimas7@gmail.com>
2020-11-29 23:53:33 +0100

    Properties #12: Yaml application skeleton

---
+++ properties/src/main/java/pl/klimas7/spring/properties/yaml/YamlApplication.java
@@ -0,0 +1,16 @@
+package pl.klimas7.spring.properties.yaml;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+public class YamlApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YamlApplication.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
