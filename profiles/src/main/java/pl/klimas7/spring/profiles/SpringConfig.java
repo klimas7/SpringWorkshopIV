c79aa2d2fda5fe0ade34e1f2ccd917870ddca14b
klimas7 <klimas7@gmail.com>
2020-11-29 16:41:32 +0100

    profiles #6: Try to add specific property files

--- profiles/src/main/java/pl/klimas7/spring/profiles/SpringConfig.java
+++ profiles/src/main/java/pl/klimas7/spring/profiles/SpringConfig.java
@@ -1,30 +1,36 @@
 package pl.klimas7.spring.profiles;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
-
 
 @Configuration
 @PropertySource("classpath:application.properties")
+@PropertySource("classpath:application-dev.properties")
+@PropertySource("classpath:application-test.properties")
 public class SpringConfig {
+
+    @Value("${extraMessage}")
+    private String extraMessage;
+
     @Bean(name = "message")
     @Lazy
     @Profile("test")
     public Message messageTest() {
-        return new MessageImpl("SimpleBeanInTest");
+        return new MessageImpl("SimpleBeanInTest: " + extraMessage);
     }
 
     @Bean(name = "message")
     @Lazy
     @Profile("dev")
     public Message message() {
-        return new MessageImpl("SimpleBean");
+        return new MessageImpl("SimpleBean: " + extraMessage);
     }
 
     @Bean(name = "message")
     @Lazy
     @Profile("default")
     public Message messageDefault() {
-        return new MessageImpl("SimpleBeanInDefault");
+        return new MessageImpl("SimpleBeanInDefault: " + extraMessage);
     }
 
     /*
@@ -51,3 +57,4 @@ public class SpringConfig {
     //Exception in thread "main" org.springframework.beans.factory.NoSuchBeanDefinitionException: No bean named 'message' available
     //Ponieważ oba beany mają profile a nie ustawiliśmy żadnego wiec domyślnie jest 'default'
 }
+
