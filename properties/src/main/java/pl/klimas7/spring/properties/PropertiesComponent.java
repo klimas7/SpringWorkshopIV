7b63421d285464130754b19042600d8cbeffa5bf
klimas7 <klimas7@gmail.com>
2020-11-29 22:53:57 +0100

    Properties #10: Other SpEL example

--- properties/src/main/java/pl/klimas7/spring/properties/PropertiesComponent.java
+++ properties/src/main/java/pl/klimas7/spring/properties/PropertiesComponent.java
@@ -69,6 +69,9 @@ public class PropertiesComponent {
     @Value("#{propertyGenerator.generate('${klimas7.spring.p6}')}")
     private Map<String, String> p6;
 
+    @Value("#{${klimas7.spring.p6a}}")
+    private Map<String, String> p6a;
+
     public PropertiesComponent(Environment env) {
         this.env = env;
     }
