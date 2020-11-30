4ce7e1bbeb490ac422ad5235fe38e73cdbf63583
klimas7 <klimas7@gmail.com>
2020-12-01 00:24:20 +0100

    jmx #4: Set as command line

--- jmx/src/main/java/pl/klimas7/spring/jmx/JmxConfiguration.java
+++ jmx/src/main/java/pl/klimas7/spring/jmx/JmxConfiguration.java
@@ -3,30 +3,39 @@ package pl.klimas7.spring.jmx;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jmx.export.MBeanExporter;
-import org.springframework.jmx.support.ConnectorServerFactoryBean;
-import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
 
-import javax.management.MalformedObjectNameException;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class JmxConfiguration {
 
-    @Bean
-    public RmiRegistryFactoryBean rmiRegistryFactoryBean() {
-        RmiRegistryFactoryBean rrfb = new RmiRegistryFactoryBean();
-        rrfb.setPort(9001);
-        return rrfb;
-    }
+    /*
+    Or In VM options:
+    -Dcom.sun.management.jmxremote=true
+    -Dcom.sun.management.jmxremote.port=9001
+    -Dcom.sun.management.jmxremote.host=localhost
+    -Dcom.sun.management.jmxremote.ssl=false
+    -Dcom.sun.management.jmxremote.authenticate=false
+    -Djava.rmi.server.hostname=127.0.0.1
 
-    @Bean
-    public ConnectorServerFactoryBean connectorServerFactoryBean() throws MalformedObjectNameException {
-        ConnectorServerFactoryBean csfb = new ConnectorServerFactoryBean();
-        csfb.setObjectName("connector:name=rmi");
-        csfb.setServiceUrl("service:jmx:rmi://localhost/jndi/rmi://localhost:9001/jmxApplication");
-        return csfb;
-    }
+    hostname -i
+     */
+
+//    @Bean
+//    public RmiRegistryFactoryBean rmiRegistryFactoryBean() {
+//        RmiRegistryFactoryBean rrfb = new RmiRegistryFactoryBean();
+//        rrfb.setPort(9001);
+//        return rrfb;
+//    }
+//
+//    @Bean
+//    public ConnectorServerFactoryBean connectorServerFactoryBean() throws MalformedObjectNameException {
+//        ConnectorServerFactoryBean csfb = new ConnectorServerFactoryBean();
+//        csfb.setObjectName("connector:name=rmi");
+//        csfb.setServiceUrl("service:jmx:rmi://localhost/jndi/rmi://localhost:9001/jmxApplication");
+//        return csfb;
+//    }
 
     @Bean
     public MBeanExporter mBeanExporter(MessageManageOperation messageManageOperation) {
