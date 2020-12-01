eaefd4977f43e449668274e921d92cef5d75aeb2
klimas7 <klimas7@gmail.com>
2020-12-01 21:26:11 +0100

    jmx #6: hide operations part 1

--- jmx/src/main/java/pl/klimas7/spring/jmx/MessageManageOperationImpl.java
+++ jmx/src/main/java/pl/klimas7/spring/jmx/MessageManageOperationImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 public class MessageManageOperationImpl implements MessageManageOperation {
     private static final String DEFAULT_MESSAGE = "Hello Sii People";
     private String message = DEFAULT_MESSAGE;
+    private String privateMessage = DEFAULT_MESSAGE;
 
     @Override
     public void setMessage(String message) {
@@ -15,5 +16,13 @@ public class MessageManageOperationImpl implements MessageManageOperation {
     @Override
     public String getMessage() {
         return message;
+    }
+
+    public String getPrivateMessage() {
+        return privateMessage;
+    }
+
+    public void setPrivateMessage(String privateMessage) {
+        this.privateMessage = privateMessage;
     }
 }
