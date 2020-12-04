3f17e9415f69e86853dcf5b4160ca0624463f196
klimas7 <klimas7@gmail.com>
2020-12-04 09:58:12 +0100

    Mongo #10: spring-boot-starter-data-rest

--- mongo/src/main/java/pl/klimas7/spring/mongo/UserMongoRepository.java
+++ mongo/src/main/java/pl/klimas7/spring/mongo/UserMongoRepository.java
@@ -1,11 +1,14 @@
 package pl.klimas7.spring.mongo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface UserMongoRepository extends MongoRepository<User, String> {
-    List<User> findUserByFirstName(String name);
-    List<User> findByFirstName(String name);
-    User findTopByAgeIsAfterOrderByAgeDesc(Integer age);
+    List<User> findUserByFirstName(@Param("name") String name);
+    List<User> findByFirstName(@Param("name") String name);
+    User findTopByAgeIsAfterOrderByAgeDesc(@Param("age") Integer age);
 }
