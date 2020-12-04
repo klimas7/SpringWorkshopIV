307a68c8d456ac6ea8b0b549972676ebd9fb0210
klimas7 <klimas7@gmail.com>
2020-12-04 08:26:08 +0100

    Mongo #1: Empty application

---
+++ mongo/src/main/java/pl/klimas7/spring/mongo/MongoApplication.java
@@ -0,0 +1,11 @@
+package pl.klimas7.spring.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MongoApplication.class);
+    }
+}
